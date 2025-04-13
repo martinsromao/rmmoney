85fc4c076368d92eaceeb62c2be94af0538a0d6e
ronivelton martins romão <roniveltonmartins2022@gmail.com>
2025-04-13 12:47:11 -0400

    add dashboard

---
+++ src/components/Dashboard/Summary/index.tsx
@@ -0,0 +1,31 @@
+import { Container } from "./style";
+import income from '../../../assets/income.svg'
+import outcome from '../../../assets/outcome.svg'
+import totalImg from '../../../assets/total.svg'
+export function Summary() {
+  return (
+    <Container >
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={income} alt="Entradas" />
+        </header>
+        <strong>R$ 1000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saída</p>
+          <img src={outcome} alt="Saída" />
+        </header>
+        <strong> - R$ 500,00</strong>
+      </div>
+      <div className="line-cor">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Entradas" />
+        </header>
+        <strong>R$ 500,00</strong>
+      </div>
+    </Container>
+  );
+}
